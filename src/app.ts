dea60385c0d84bfbd983a9ffe12a8f844331b0ea
Nur Alom <md9577630@gmail.com>
2026-01-27 20:15:21 +0600

    add user model

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { auth } from './lib/auth';
+import { toNodeHandler } from "better-auth/node";
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: process.env.APP_URL || 'http://localhost:3000',
+    credentials: true,
+}));
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.get('/', (req, res) => {
+    res.send(`Food Hub server is running on Port: ${process.env.PORT || 5000}`);
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const port = process.env.PORT || 5000;
+
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("Connected to the database successfully.");
 
 
-const app = express();
-app.use(express.json());
-app.use(cors({
-    origin: process.env.APP_URL || 'http://localhost:3000',
-    credentials: true,
-}));
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+    catch (err) {
+        console.error("Failed to connect to the database:", err);
+        prisma.$disconnect();
+        process.exit(1);
+    }
+};
 
 
-
-
-
-// ! Default Get.
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome To Food Hub Server.')
-});
-
-
-// ! Server Listing.
-app.listen(5000, () => {
-    console.log(`Food Hub Server listening on port 5000`)
-});
+main();
